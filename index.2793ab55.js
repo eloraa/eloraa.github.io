8b80e0d9113f8ebb220dafff9444241705cb2ee1
Neon <56364111+eloraa@users.noreply.github.com>
2023-06-26 02:54:20 +0600

    Update index.2793ab55.js

--- index.2793ab55.js
+++ index.2793ab55.js
@@ -1,1 +1,11 @@
-class App{constructor(){window.addEventListener("load",(()=>{this.preloader=document.querySelector(".preloader"),this.preloader.style.opacity="0",setTimeout((()=>{this.preloader.remove()}),1e3)}))}}new App;
+! function() {
+    'use strict'
+    const color = (Math.floor(Math.random() * 46) + 210).toString(16) + (Math.floor(Math.random() * 46) + 210).toString(16) + (Math.floor(Math.random() * 46) + 210).toString(16);
+    const dark = (Math.round((parseInt(color.substring(0, 2), 16)) * .75)).toString(16).padStart(2, "0") + (Math.round((parseInt(color.substring(2, 4), 16)) * .75)).toString(16).padStart(2, "0") + (Math.round((parseInt(color.substring(4, 6), 16)) * .75)).toString(16).padStart(2, "0");
+    const dark_ooooooooo = (Math.round((parseInt(color.substring(0, 2), 16)) * .45)).toString(16).padStart(2, "0") + (Math.round((parseInt(color.substring(2, 4), 16)) * .45)).toString(16).padStart(2, "0") + (Math.round((parseInt(color.substring(4, 6), 16)) * .45)).toString(16).padStart(2, "0");
+
+
+    document.documentElement.style.setProperty('--secondary', '#' + color);
+    document.documentElement.style.setProperty('--accent', '#' + dark);
+    document.documentElement.style.setProperty('--third', '#' + dark_ooooooooo);
+}()
